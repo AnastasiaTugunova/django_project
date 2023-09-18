85a99cadd92a3e16f91e42aea41a02059b857507
Tugunova Anastasia <annamir7516@gmail.com>
2023-09-18 13:33:49 +0700

    add ex.4

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
-from articles.models import Article
+from .models import Article
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    article_list = Article.objects.all()
+    context = {
+        'object_list': article_list
+    }
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
