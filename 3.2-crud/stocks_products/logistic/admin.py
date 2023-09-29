502732f7a0e505f02ee85934aad8529c1774c316
Tugunova Anastasia <annamir7516@gmail.com>
2023-09-29 15:49:23 +0700

    ex.6

--- 3.2-crud/stocks_products/logistic/admin.py
+++ 3.2-crud/stocks_products/logistic/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Stock
+
+admin.site.register(Product)
+admin.site.register(Stock)
--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,29 @@
 from rest_framework import serializers
-
+from logistic.models import Product, Stock, StockProduct
+from rest_framework.exceptions import ValidationError
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = '__all__'
 
+        def validate_text(self, value):
+            if 'product' in value:
+                raise ValidationError('Неверное название продукта')
+            return value
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['id', 'product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -23,10 +32,9 @@ class StockSerializer(serializers.ModelSerializer):
         # создаем склад по его параметрам
         stock = super().create(validated_data)
 
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for position in positions:
+            stock_create = StockProduct.objects.create(stock=stock, **position)
+            stock_create.save()
         return stock
 
     def update(self, instance, validated_data):
@@ -36,8 +44,16 @@ class StockSerializer(serializers.ModelSerializer):
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for position in positions:
+            up_stock_product = StockProduct.objects.update_or_create(
+                stock=stock,
+                product=position['product'],
+                defaults={
+                    'stock': stock,
+                    'product': position['product'],
+                    'quantity': position['quantity'],
+                    'price': position['price']
+                }
+            )
+            up_stock_product.save()
         return stock
