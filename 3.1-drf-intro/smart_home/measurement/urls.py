7d745dbea3ef6d6a191b4686a3b1b74489aaa695
Anastasia Tugunova <127911920+AnastasiaTugunova@users.noreply.github.com>
2023-11-07 17:07:19 +0700

    Delete 3.1-drf-intro directory

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CreateSensorView, MeasurementCreateView, SensorChangeView
-
-urlpatterns = [
-    path('sensors/', CreateSensorView.as_view()),
-    path('sensors/<pk>/', SensorChangeView.as_view()),
-    path('measurements/', MeasurementCreateView.as_view()),
-]
